6d01d04c720a353809a20c03871e58f967d5fac8
Ashik Meerankutty <ashik9591@gmail.com>
2020-10-24 23:50:44 +0530

    Add action bar and grid

---
+++ src/app/pages/editor/actionbar.tsx
@@ -0,0 +1,126 @@
+/**@jsx jsx */
+import { css, jsx } from '@emotion/core';
+import {
+  IconButton,
+  DoubleChevronLeftIcon,
+  DoubleChevronRightIcon,
+} from 'evergreen-ui';
+import { FunctionComponent, useState } from 'react';
+
+const actionBarStyles = (shown: boolean) => css`
+  height: calc(100vh - 64px);
+  padding: 30px 0px;
+  width: ${shown ? '380px' : '0px'};
+  position: relative;
+  border-right: 1px solid #e8e8ef;
+  border-bottom: 1px solid #e8e8ef;
+  transition: width 0.3s;
+  .actionbar__content {
+    max-width: 100%;
+    transition: 0.1s ease;
+    transform: ${shown ? 'translateX(0)' : 'translateX(-100%)'};
+    overflow: hidden;
+  }
+  .actionbar__hide {
+    position: absolute;
+    right: -40px;
+    top: 20px;
+  }
+  .actionBar__title {
+    margin: 20px;
+  }
+`;
+
+const tabStyles = (selected: boolean) => css`
+  cursor: pointer;
+  .tab__title {
+    color: ${selected ? 'rgb(23,43,77)' : '#808292'};
+    border-bottom: ${selected ? '4px solid #47B881' : 'none'};
+    font-weight: 400;
+    padding: 10px 10px;
+  }
+  &:hover {
+    .tab__title {
+      color: rgb(23, 43, 77);
+    }
+  }
+`;
+
+const tabsHolderStyles = css`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 20px;
+  border-bottom: 1px solid #e8e8ef;
+`;
+
+const tabsDataHolderStyles = css`
+  padding: 20px;
+`;
+
+const tabDataContentStyles = (selected: boolean) => css`
+  display: ${selected ? 'block' : 'none'};
+  .tabdata__action {
+    padding-bottom: 10px;
+  }
+`;
+
+interface ActionBarProps {}
+
+const tabs = ['Triggers', 'Actions', 'Loggers'];
+
+const actions = [
+  ['Trigger1', 'Trigger2', 'Trigger3'],
+  ['Action1', 'Action2', 'Action3'],
+  ['Loggers1', 'Loggers2', 'Loggers3'],
+];
+
+const ActionBar: FunctionComponent<ActionBarProps> = () => {
+  const [isActionBarShown, setIsActionBarShown] = useState(true);
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
+
+  return (
+    <div css={actionBarStyles(isActionBarShown)}>
+      <div className="actionbar__hide">
+        <IconButton
+          height={40}
+          onClick={() => setIsActionBarShown(!isActionBarShown)}
+          icon={isActionBarShown ? DoubleChevronLeftIcon : DoubleChevronRightIcon}
+        />
+      </div>
+      <div className="actionbar__content">
+        <h3 className="actionBar__title">Action blocks</h3>
+        <div>
+          <div css={tabsHolderStyles}>
+            {tabs.map((tab, index) => (
+              <div
+                css={tabStyles(index === selectedTabIndex)}
+                key={tab}
+                onClick={() => {
+                  setSelectedTabIndex(index);
+                }}
+              >
+                <h4 className="tab__title">{tab}</h4>
+              </div>
+            ))}
+          </div>
+          <div css={tabsDataHolderStyles}>
+            {actions.map((action, index) => (
+              <div
+                key={`action-${index}`}
+                css={tabDataContentStyles(index === selectedTabIndex)}
+              >
+                {action.map((a) => (
+                  <div className="tabdata__action" key={a}>
+                    {a}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActionBar;
--- src/app/pages/editor/index.tsx
+++ src/app/pages/editor/index.tsx
@@ -1,10 +1,11 @@
 /**@jsx jsx */
 import { useQuery } from '@apollo/client';
-import { jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/core';
 import { FunctionComponent, useEffect, useState } from 'react';
 import { Loading } from '../../shared/components/loading';
 import { Navbar } from '../../shared/components/navbar';
 import { GET_WORKFLOW } from './query';
+import ActionBar from './actionbar';
 
 interface EditorProps {
   match: {
@@ -13,6 +14,15 @@ interface EditorProps {
     };
   };
 }
+
+const editorStyles = css`
+  display: flex;
+  .editor__main {
+    background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAH0lEQVQoU2NkYGD4z8DAwMhAABBUANM/qhBvSBIdPACgdAELiyknowAAAABJRU5ErkJggg==)
+      repeat;
+    width: 100%;
+  }
+`;
 
 const Editor: FunctionComponent<EditorProps> = ({ match }: EditorProps) => {
   const [workflow, setWorkflow] = useState(null);
@@ -59,6 +69,10 @@ const Editor: FunctionComponent<EditorProps> = ({ match }: EditorProps) => {
         onSave={() => {}}
         onDiscard={() => {}}
       />
+      <div css={editorStyles}>
+        <ActionBar />
+        <div className="editor__main"></div>
+      </div>
     </div>
   );
 };
