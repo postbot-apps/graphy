cf6003f687ad404d300fbb91f25a7a2af7ec9012
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-01 15:07:26 +0530

    Removed script

--- scripts/create-env.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-const fs = require('fs');
-fs.writeFileSync('./.env', `SLASH_GRAPHQL_URL=${process.env.SLASH_GRAPHQL_URL}\n`);
-fs.writeFileSync('./.env', `AUTH0_DOMAIN=${process.env.AUTH0_DOMAIN}\n`);
-fs.writeFileSync('./.env', `AUTH0_CLIENT_ID=${process.env.AUTH0_CLIENT_ID}\n`);
-fs.writeFileSync('./.env', `AUTH0_AUDIENCE=${process.env.AUTH0_AUDIENCE}\n`);
-fs.writeFileSync('./.env', `APP_ENDPOINT=${process.env.APP_ENDPOINT}\n`);
